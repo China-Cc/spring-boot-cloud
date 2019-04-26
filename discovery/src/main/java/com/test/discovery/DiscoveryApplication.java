13d1930678a10a5143068075a5d08ea85ed958b6
348757 <chencheng056@deppon.com>
2019-04-26 18:22:39 +0800

    更换内置启动服务器为jetty

--- discovery/src/main/java/com/test/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/test/discovery/DiscoveryApplication.java
@@ -7,7 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
+
+    // 程序的入口，启动加载配置，初始化容器
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
     }
+
 }
